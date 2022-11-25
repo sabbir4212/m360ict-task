a639e326624083c1407b31025a047a39432b4955
sabbir322 <sabbirhosan370@gmail.com>
2022-11-26 00:25:46 +0600

    upcoming filtering done

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -10,7 +10,6 @@ const Main: React.FC = () => {
   const dispatch = useDispatch();
   const [searched, setSearched] = useState([]);
   const [filterd, setFilterd] = useState([]);
-  const [loading, setLoading] = useState(false);
   const flights = useSelector((state: any) => state.flights);
   const { flight, isLoading } = flights;
 
@@ -19,23 +18,19 @@ const Main: React.FC = () => {
   }, [dispatch]);
 
   useEffect(() => {
-    setLoading(true);
     setSearched(flight);
-    setLoading(false);
   }, [flight]);
 
   useEffect(() => {
-    setLoading(true);
     setSearched(filterd);
-    setLoading(false);
   }, [filterd]);
 
-  if (isLoading || loading) {
+  if (isLoading) {
     return <Loading></Loading>;
   }
 
   // search event
-  const onSearch = (e: any): void => {
+  const onSearch = (e: string): void => {
     const filtered = filterd.filter((searchedFlight: any) =>
       searchedFlight.rocket?.rocket_name.includes(e)
     );
@@ -43,22 +38,17 @@ const Main: React.FC = () => {
   };
 
   // sort by date
-  const sortByDate = (e: any) => {
+  const sortByDate = (e: string): void => {
     if (e) {
       if (e === "All") {
-        setLoading(true);
         setFilterd(flight);
-        setLoading(false);
       } else if (e === "lastYear") {
-        setLoading(true);
         const lastYearDate = flight.slice(-1)[0].launch_year;
         const lastYears = flight.filter(
           (ltyr: any) => parseInt(ltyr.launch_year) === parseInt(lastYearDate)
         );
         setFilterd(lastYears);
-        setLoading(false);
       } else if (e === "lastMonth") {
-        setLoading(true);
         for (const unix of flight) {
           const lastLaunchedDate = unix.launch_date_local.split("T")[0];
           const makeDate = new Date(lastLaunchedDate);
@@ -81,11 +71,8 @@ const Main: React.FC = () => {
           );
 
           setFilterd(filterLastMonths);
-          setLoading(false);
         }
       } else if (e === "lastWeek") {
-        setLoading(true);
-
         for (const unix of flight) {
           const lastLaunchedDate = unix.launch_date_local.split("T")[0];
           const makeDate = new Date(lastLaunchedDate);
@@ -94,14 +81,12 @@ const Main: React.FC = () => {
             makeDate.getMonth(),
             makeDate.getDate() - 7
           );
-          const prevDate: any = prev.toLocaleDateString().split("/");
+          const prevDate: string[] = prev.toLocaleDateString().split("/");
           const filterLastWeeks = flight.filter(
             (filterLastWeek: any) =>
               new Date(filterLastWeek.launch_date_local)
                 .toLocaleDateString()
-                .split("/")[1] +
-                7 ===
-                prevDate[1] &&
+                .split("/")[1] <= prevDate[1] &&
               new Date(filterLastWeek.launch_date_local)
                 .toLocaleDateString()
                 .split("/")[2] === prevDate[2] &&
@@ -111,16 +96,48 @@ const Main: React.FC = () => {
           );
           setFilterd(filterLastWeeks);
         }
-        setLoading(false);
       }
     }
   };
+
+  // sort by launching status
+  const sortByLaunchingStatus = (e:string):void => {
+    if(e){
+      if (e === "All") {
+        setFilterd(flight);
+      }
+      else if(e === "success"){
+        console.log('success')
+      }
+      else if(e === 'fail'){
+        console.log('fail')
+      }
+    }
+  }
+
+  // sort by is upcoming 
+  const sortByUpcoming = (e:string):void => {
+    if(e){
+      if (e === "All") {
+        setFilterd(flight);
+      }
+      else if(e === 'upcomimg'){
+        const filterUpcoming = flight.filter((upcoming:any) => upcoming.upcoming === true);
+        setFilterd(filterUpcoming)
+      }
+      else if(e === 'notUpcoming'){
+        const filterNotUpcomint = flight.filter((notUpcomint:any) => notUpcomint.upcoming === false);
+        setFilterd(filterNotUpcomint)
+      }
+    }
+  }
 
   return (
     <div>
       <div className="" style={{ margin: "0 auto", padding: "20px" }}>
         <Row justify="space-between">
           <Col>
+        <p style={{margin:'0'}}>Search by rocket name</p>
             <Space direction="vertical">
               <Search
                 placeholder="Search by rocket name"
@@ -131,6 +148,7 @@ const Main: React.FC = () => {
             </Space>
           </Col>
           <Col>
+          <p style={{margin:'0'}}>Select launching dates</p>
             <Select
               defaultValue="All"
               style={{ width: 300 }}
@@ -153,6 +171,56 @@ const Main: React.FC = () => {
                   value: "lastYear",
                   label: "Last Year",
                 },
+              ]}
+            />
+          </Col>
+
+          {/* launching status select */}
+          <Col>
+          <p style={{margin:'0'}}>Select launching status</p>
+            <Select
+              defaultValue="All"
+              style={{ width: 300 }}
+              allowClear
+              onChange={sortByLaunchingStatus}
+              options={[
+                {
+                  value: "All",
+                  label: "All",
+                },
+                {
+                  value: "success",
+                  label: "Launching success",
+                },
+                {
+                  value: "fail",
+                  label: "Launching fail",
+                }
+              ]}
+            />
+          </Col>
+          
+          {/* upcoming select */}
+          <Col>
+          <p style={{margin:'0'}}>Select is upcoming</p>
+            <Select
+              defaultValue="All"
+              style={{ width: 300 }}
+              allowClear
+              onChange={sortByUpcoming}
+              options={[
+                {
+                  value: "All",
+                  label: "All",
+                },
+                {
+                  value: "upcomimg",
+                  label: "Upcoming",
+                },
+                {
+                  value: "notUpcoming",
+                  label: "Not Upcoming",
+                }
               ]}
             />
           </Col>
--- src/utils/type/flightTypes.ts
+++ src/utils/type/flightTypes.ts
@@ -1,31 +1,31 @@
 export interface flightType {
-  crew: String | null;
-  details: String | null;
-  flight_number: Number|any;
-  is_tentative: Boolean;
-  launch_date_local: String;
-  launch_date_unix: Number;
-  launch_date_utc: String;
-  launch_site: {};
-  launch_success: Boolean;
-  launch_window: Number | null;
-  launch_year: String | Number | null;
-  links: {}| any;
+  crew: string | null;
+  details: string | null;
+  flight_number: number|any;
+  is_tentative: boolean;
+  launch_date_local: string;
+  launch_date_unix: number;
+  launch_date_utc: string;
+  launch_site: any;
+  launch_success: boolean|number;
+  launch_window: number | null;
+  launch_year: string | number | null;
+  links:  any;
   mission_id: [];
-  mission_name: String;
-  rocket: {}|any;
-  ships: [] | String | null;
-  static_fire_date_unix: Number | null;
-  static_fire_date_utc: String;
-  tbd: Boolean;
-  telemetry: {};
-  tentative_max_precision: String | null;
-  timeline: {};
-  upcoming: Boolean | null;
+  mission_name: string;
+  rocket: any;
+  ships: [] | string | null;
+  static_fire_date_unix: number | null;
+  static_fire_date_utc: string;
+  tbd: boolean;
+  telemetry: any;
+  tentative_max_precision: string | null;
+  timeline: string;
+  upcoming: boolean | null;
 }
 
 export interface IFlightState {
   user: flightType | null | [] | any;
-  isLoading: Boolean;
+  isLoading: boolean;
   error: any;
 }
